ff5a2efa85bea81ee5e9aaa15080c95f698ba21d
Arturas Utakis <utakis.arturas@gmail.com>
2018-07-10 21:31:44 +0300

    uzduotys

---
+++ ConsoleApp1/Kepykla.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Kepykla
+    {
+        public string Pavadinimas { get; private set; }
+        public string Adresas { get; private set; }
+        public List<Duona> Duonos { get; private set; }
+        public Kepykla()
+        {
+            Duonos = new List<Duona>();
+        }
+        public Kepykla(string pavadinimas,string adresas, List<Duona> duonos)
+        {
+            Pavadinimas = pavadinimas;
+            Adresas = adresas;
+            Duonos = duonos;
+        }
+        public void Isvedimas()
+        {
+            Console.WriteLine("Kepyklos pavadinimas : {0} adresu {1}",Pavadinimas,Adresas);
+
+            foreach (var duona in Duonos)
+            {
+                duona.Isvedimas();
+            }
+        }
+    }
+}
